896faded2644837a64ffd6074a3a28ed2709f694
dudghks117 <dudghks117@naver.com>
2020-06-15 12:30:56 +0900

    Generic Source 수정

--- GenericApp/Program.cs
+++ GenericApp/Program.cs
@@ -6,11 +6,42 @@ using System.Threading.Tasks;
 
 namespace GenericApp
 {
+    public class SimpleGeneric<T>
+    {
+        private T[] values;
+        private int index;
+
+        public SimpleGeneric(int len)
+        {
+            values = new T[len];
+            index = 0;
+        }
+        public void Add(params T[] args)
+        {
+            foreach (T item in args)
+                values[index++] = item;
+        }
+        public void Print()
+        {
+            foreach (T item in values)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ㅎㅇ");
+            SimpleGeneric<Int32> gInteger = new SimpleGeneric<Int32>(10);
+            SimpleGeneric<Double> gDouble = new SimpleGeneric<Double>(10);
+
+            gInteger.Add(1, 2);
+            gInteger.Add(1, 2, 3, 4, 5, 6, 7);
+            gInteger.Add(0);
+            gInteger.Print();
+
+            gDouble.Add(10.0, 20.1, 33.5);
+            gDouble.Print();
         }
     }
 }
